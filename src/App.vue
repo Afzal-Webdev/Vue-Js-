861d64e55f1b9d2435d2687c28f6950cd931e050
afzal <afzal.appelit@nextbridge.com>
2024-10-17 13:42:23 +0500

    First Commit Here -- Vue Assignemnet

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+
+<style>
+@import './assets/main.css'; /* Import Tailwind */
+</style>
+<template>
+  <div id="app">
+    <section class="container mx-auto">
+      <UserList />
+    </section>
+   
+  </div>
+</template>
+
+<script>
+import UserList from './components/UserList.vue';
+
+export default {
+  components: {
+    UserList,
+  },
+};
+</script>
+
+
